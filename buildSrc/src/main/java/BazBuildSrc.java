964677911d380a8460b8244c026edbd930d14e84
Andrei Rybak <rybak.a.v@gmail.com>
2019-05-03 13:55:42 +0200

    Gradle: buildSrc example

---
+++ buildSrc/src/main/java/BazBuildSrc.java
@@ -0,0 +1,5 @@
+public class BazBuildSrc {
+	public static void test() {
+		System.out.println("Test. 1-2-3");
+	}
+}
---
+++ buildSrc/src/main/java/dev/andrybak/FoobarBuildSrc.java
@@ -0,0 +1,7 @@
+package dev.andrybak;
+
+public class FoobarBuildSrc {
+	public static void hello() {
+		System.out.println("Hello, World!");
+	}
+}
