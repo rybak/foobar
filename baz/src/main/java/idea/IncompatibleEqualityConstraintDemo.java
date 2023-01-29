a589ae643b6116339659d60dc86d098589d57eb2
Andrei Rybak <rybak.a.v@gmail.com>
2023-01-29 15:52:35 +0100

    IncompatibleEqualityConstraintDemo: IntelliJ IDEA bug demo

---
+++ baz/src/main/java/idea/IncompatibleEqualityConstraintDemo.java
@@ -0,0 +1,39 @@
+package idea;
+
+import java.util.function.Function;
+
+class IncompatibleEqualityConstraintDemo {
+	static void bar() {
+		Foo<String, Integer> fooStringInteger = new Foo<>();
+		FooCorrected<String, Integer> fooCorrected = new FooCorrected<>();
+		{
+			Function<CharSequence, String> badFunctionA = cs -> "badFunctionA" + cs.toString();
+
+			// doesn't compile because CharSequence != String
+			fooStringInteger.foo(badFunctionA, i -> "Good function" + i.toString());
+
+			// compiles because CharSequence satisfies constraint `? super String`
+			fooCorrected.foo(badFunctionA, i -> "Good function" + i.toString());
+		}
+		{
+			// Integer extends Number
+			Function<Number, String> badFunctionB = n -> n.toString() + "g";
+
+			// doesn't compile because Number != Integer
+			fooStringInteger.foo(s -> "Good function" + s, badFunctionB);
+
+			// compiles because Number satisfies constraint `? super Integer`
+			fooCorrected.foo(s -> "Good function" + s, badFunctionB);
+		}
+	}
+
+	static class Foo<A, B> {
+		<R> void foo(Function<A, R> a, Function<B, R> b) {
+		}
+	}
+
+	static class FooCorrected<A, B> {
+		<R> void foo(Function<? super A, R> a, Function<? super B, R> b) {
+		}
+	}
+}
