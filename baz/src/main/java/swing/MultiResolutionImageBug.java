4d6306d69a0059c6e697e73ea670a647d5d3762a
Andrei Rybak <rybak.a.v@gmail.com>
2023-07-15 00:36:33 +0200

    MultiResolutionImageBug: more explanation

--- baz/src/main/java/swing/MultiResolutionImageBug.java
+++ baz/src/main/java/swing/MultiResolutionImageBug.java
@@ -13,16 +13,18 @@ import java.util.Objects;
 /**
  * Tested on:
  * - Linux:
- *   - Java 11.0.19         ❌ Fail
- *   - Java 17.0.7          ❌ Fail
- *   - Java 19.0.2          ❌ Fail
- *   - Java 20.0.1-testing  ❌ Fail
+ * - Java 11.0.19         ❌ Fail
+ * - Java 17.0.7          ❌ Fail
+ * - Java 19.0.2          ❌ Fail
+ * - Java 20.0.1-testing  ❌ Fail
  * - Windows:
  * -
  * - macOS:
  * -
  */
 public class MultiResolutionImageBug {
+	public static final String ICON_64 = "icon64x64.png";
+	public static final String ICON_32 = "icon32x32.png";
 	private int attemptsCounter;
 	private JLabel counterLabel;
 	private JFrame mainWindow;
@@ -35,17 +37,27 @@ public class MultiResolutionImageBug {
 	 * On Linux, first attempt always fails, because image isn't loaded yet,
 	 * and {@code sun.awt.IconInfo#isValid} returns {@code false}.
 	 */
-	private void showDialog(JFrame mainWindow) {
+	private void showDialog(JFrame mainWindow, boolean multi) {
 		attemptsCounter++;
 		counterLabel.setText(getLabelText());
 		JDialog dialog = new JDialog(mainWindow, "Dialog attempt #" + attemptsCounter, Dialog.ModalityType.MODELESS);
 		dialog.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
-		dialog.setIconImage(getMultiResolutionImage());
+
+		dialog.setIconImage(multi ? getMultiResolutionImage() : getFixedResolutionImage());
+
 		dialog.setLocation(200, 200);
 		dialog.setSize(400, 400);
-		JTextArea textDisplay = new JTextArea(
-				"On the first attempt, the default Duke icon will be shown on Linux."
-		);
+		final String explanation;
+		if (!multi) {
+			explanation = "Fixed resolution works fine.";
+		} else {
+			if (attemptsCounter == 1) {
+				explanation = "On Linux, on the first attempt, the Duke icon is shown.";
+			} else {
+				explanation = "On subsequent attempts, the correct icon is shown.";
+			}
+		}
+		JTextArea textDisplay = new JTextArea(explanation);
 		dialog.getContentPane().add(textDisplay);
 		setUpEscapeKeyClosing(dialog, textDisplay);
 		dialog.setVisible(true);
@@ -55,15 +67,18 @@ public class MultiResolutionImageBug {
 		attemptsCounter = 0;
 		mainWindow = new JFrame("Bug demo on Java version " + System.getProperty("java.version"));
 		mainWindow.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-		JPanel contentPane = new JPanel(new BorderLayout());
+		JPanel contentPane = new JPanel(new GridLayout(3, 1));
 		mainWindow.setContentPane(contentPane);
 
 		counterLabel = new JLabel(getLabelText());
-		contentPane.add(counterLabel, BorderLayout.NORTH);
+		contentPane.add(counterLabel);
 
-		JButton showDialogButton = new JButton("Show dialog with icon");
-		showDialogButton.addActionListener(ignored -> showDialog(mainWindow));
-		contentPane.add(showDialogButton, BorderLayout.CENTER);
+		JButton showMultiIconDialogButton = new JButton("Show dialog with BaseMultiResolutionImage");
+		showMultiIconDialogButton.addActionListener(ignored -> showDialog(mainWindow, true));
+		contentPane.add(showMultiIconDialogButton);
+		JButton showFixedIconDialogButton = new JButton("Show dialog with fixed resolution Image");
+		showFixedIconDialogButton.addActionListener(ignored -> showDialog(mainWindow, false));
+		contentPane.add(showFixedIconDialogButton);
 
 		mainWindow.setSize(400, 400);
 		mainWindow.setVisible(true);
@@ -75,9 +90,13 @@ public class MultiResolutionImageBug {
 	}
 
 	private Image getMultiResolutionImage() {
-		var icon32 = getFixedResolutionImage("icon32x32.png");
-		var icon64 = getFixedResolutionImage("icon64x64.png");
+		Image icon32 = getFixedResolutionImage(ICON_32);
+		Image icon64 = getFixedResolutionImage(ICON_64);
 		return new BaseMultiResolutionImage(icon32, icon64);
+	}
+
+	private Image getFixedResolutionImage() {
+		return getFixedResolutionImage(ICON_64);
 	}
 
 	private Image getFixedResolutionImage(String filename) {
